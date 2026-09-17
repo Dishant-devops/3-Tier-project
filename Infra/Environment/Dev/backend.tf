ff4fb9c73d5ad37c854b97e6a6418b3fce61a390
Dishant <dishantsaini203@gmail.com>
2026-09-17 23:00:15 +0530

    update

--- Infra/Environment/Dev/backend.tf
+++ Infra/Environment/Dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "my-bucket-782989862342-us-east-1-an"
-    key    = "dev/terraform.tfstate"
-    region = "us-east-1"
-    encrypt = true
-    use_lockfile = true   # S3 native locking 
+    bucket       = "my-bucket-782989862342-us-east-1-an"
+    key          = "dev/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true # S3 native locking 
   }
 }
